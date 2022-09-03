3b862170ad86016ea99e626c5ed015d81c2ff69e
NileZhou <nilezhou123@gmail.com>
2022-09-03 17:18:07 +0800

    add server module

--- lightmq-server/src/main/java/org/light/Server.java
+++ lightmq-server/src/main/java/org/light/Server.java
@@ -1,14 +1,11 @@
 package org.light;
 
-
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class Server
 {
-
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
